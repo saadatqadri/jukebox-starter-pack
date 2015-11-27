1336557aa54b1bda2a5fbb6e2515887fecac647b
achristens <achristens@Abigails-MacBook-Air.local>
2015-11-26 19:07:52 -0500

    Updated "Plus" plan song count

--- js/jukebox.js
+++ js/jukebox.js
@@ -1,4 +1,10 @@
+<<<<<<< Updated upstream
 $( document ).ready(function() {
     $('.hero-background').fadeIn(1400);
      $('.hero-background-text').fadeIn(5000);
 });
+=======
+$('carousel').carousel({
+	interval:2000
+})
+>>>>>>> Stashed changes
